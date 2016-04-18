63a3a756140c5688c0cc7048c7001c055b753b84
Jonathan Wehrend <jwehrend@gmail.com>
2016-04-18 15:34:40 -0600

    Modified scripts and script associations

--- Assets/Scripts/SpawnCoins.cs
+++ Assets/Scripts/SpawnCoins.cs
@@ -1,25 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpawnCoins : MonoBehaviour {
+public class SpawnCoins : MonoBehaviour
+{
+    public float fallSpeed = 8.0f;
+    // Optional if the object should spin while it falls
+    public float spinSpeed = 100.0f;
+    public Transform[] coinSpawns;
+    public GameObject coin;
 
-	public Transform[] coinSpawns;
-	public GameObject coin;
+    void Start()
+    {
+        Spawn();
+    }
 
-	// Use this for initialization
-	void Start () {
+    void Spawn()
+    {
+        for (int i = 0; i < coinSpawns.Length; i++)
+        {
+            int coinFlip = Random.Range(0, 2);
+            if (coinFlip > 0)
+            {
+                // Spawn new coin at top of screen
+                Instantiate(coin, new Vector3(Random.Range(-6f, 6f), 10f, 0f), Quaternion.identity);
 
-		Spawn();
-	}
+            }
+        }
+    }
 
-	void Spawn()
-	{
-		for (int i = 0; i < coinSpawns.Length; i++)
-		{
-			int coinFlip = Random.Range (0, 2);
-			if (coinFlip > 0)
-				Instantiate(coin, coinSpawns[i].position, Quaternion.identity);
-		}
-	}
+    void Update()
+    {
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+
+        // Need additional script to destroy coin  if it falls past the lower boundary on the y-axis
+    }
 
 }
