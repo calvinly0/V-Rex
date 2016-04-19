ddb90ce93eaa2bc7093e1a7cd6156b0731afd3c0
Jonathan Wehrend <jwehrend@gmail.com>
2016-04-19 14:26:08 -0600

    Updated PickupCoin script to have counter -- Counter not yet working

--- Assets/Scripts/PickupCoin.cs
+++ Assets/Scripts/PickupCoin.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PickupCoin : MonoBehaviour {
 
+	public Texture2D coinIconTexture;
+	public int coinCount = 0;
+
 	// Use this for initialization
 	void Start () {
 
@@ -16,6 +19,28 @@ public class PickupCoin : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.gameObject.CompareTag("Player"))
+		{
+			coinCount++;
 			Destroy(gameObject);
+		}
+	}
+
+	void DisplayCoinsCount()
+	{
+		Rect coinIconRect = new Rect(10, 10, 32, 32);
+		GUI.DrawTexture(coinIconRect, coinIconTexture);
+
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 30;
+		style.fontStyle = FontStyle.Bold;
+		style.normal.textColor = Color.yellow;
+
+		Rect labelRect = new Rect(coinIconRect.xMax, coinIconRect.y, 60, 32);
+		GUI.Label(labelRect, coinCount.ToString(), style);
+	}
+
+	void OnGUI()
+	{
+		DisplayCoinsCount();
 	}
 }
